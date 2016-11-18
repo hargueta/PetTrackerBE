251884b44f85b2e84a8bca58dd377fcdc3ece05b
Hugo Argueta <hugoargueta94@gmail.com>
2016-11-18 14:32:39 -0800

    Added mongodb connectivity

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+
+var urlString = process.env.MONGOLAB_URI ||
+                process.env.MONGOHQ_URL ||
+                "localhost/pettracker";
+
+mongoose.connect(urlString);
+
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,6 +21,6 @@ app.use('/', function(request, response) {
     response.json({message: 'Hello, World!'});
 })
 
-app.listen(port)
+app.listen(port);
 
-console.log('Port: ' + port);
+console.log('Listening on port ' + port);
