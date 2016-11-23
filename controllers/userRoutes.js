f4d4c596042722777268ba35b44628c63034251d
Hugo Argueta <hugoargueta94@gmail.com>
2016-11-23 15:18:17 -0800

    Added endpoint for user login.

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -33,4 +33,23 @@ router.route('/create').post(function(request, response) {
     console.log("User ID: " + user._id);
 });
 
+// Check if a user entered valid login credentials (using POST at http://localhost:8080/user/login)
+router.route('/login').post(function(request, response) {
+    var uname = request.body.username;
+    var pass = request.body.password;
+
+    User.find({username: uname, password: pass}, function(err, user) {
+        if(err) {
+            response.send(err);
+        }
+
+        if(user) {
+            response.json({result: "true"});
+        } else {
+            response.json({result: "false"});
+        }
+
+    });
+});
+
 module.exports = router;
